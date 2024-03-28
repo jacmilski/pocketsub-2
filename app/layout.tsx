f64f5ead4dbef019226ab3f694ce7599cee43750
Jacek Michalski <jacmilski@gmail.com>
2024-03-28 17:06:52 +0100

    Add prettier ro project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,18 @@
-import './globals.css'
+import './globals.css';
 
 export const metadata = {
   title: 'Pocketsub',
   description: 'Make your payments easier',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>{children}</body>
     </html>
-  )
+  );
 }
