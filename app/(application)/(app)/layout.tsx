e31ab43a64848fc0b8bf4bf0893d20ad47aae33d
Jacek Michalski <jacmilski@gmail.com>
2024-03-31 10:50:09 +0200

    Add auth flow and layout.

---
+++ app/(application)/(app)/layout.tsx
@@ -0,0 +1,11 @@
+export default function ApplicationLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <main>{children}</main>
+    </div>
+  );
+}
---
+++ app/api/webhooks/user/route.ts
@@ -0,0 +1,63 @@
+import { IncomingHttpHeaders } from "http";
+import { headers } from "next/headers";
+import { NextResponse } from "next/server";
+import { Webhook, WebhookRequiredHeaders } from "svix";
+import { prisma } from "../../../../lib/db";
+
+const webhookSecret = process.env.WEBHOOK_SECRET || "";
+
+async function handler(request: Request) {
+  const payload = await request.json();
+  const headersList = headers();
+
+  const heads = {
+    "svix-id": headersList.get("svix-id"),
+    "svix-timestamp": headersList.get("svix-timestamp"),
+    "svix-signature": headersList.get("svix-signature"),
+  };
+
+  const wh = new Webhook(webhookSecret);
+  let evt: Event | null = null;
+
+  try {
+    evt = wh.verify(
+      JSON.stringify(payload),
+      heads as IncomingHttpHeaders & WebhookRequiredHeaders,
+    ) as Event;
+
+    const eventType = evt.type;
+    if (eventType === "user.created" || eventType === "user.updated") {
+      const { id, email_addresses, username } = evt.data;
+
+      const data = await prisma.user.upsert({
+        where: { email: email_addresses[0].email_address },
+        create: {
+          email: email_addresses[0].email_address,
+          name: username,
+          userId: id,
+        },
+        update: {
+          email: email_addresses[0].email_address,
+        },
+      });
+      return NextResponse.json({ status: 200 });
+    }
+  } catch (err) {
+    console.error((err as Error).message);
+    return NextResponse.json({}, { status: 400 });
+  }
+}
+
+type Event = {
+  data: {
+    id: string;
+    email_addresses: { id: string; email_address: string }[];
+    username: string;
+  };
+  object: string;
+  type: string;
+};
+
+export const GET = handler;
+export const POST = handler;
+export const PUT = handler;
