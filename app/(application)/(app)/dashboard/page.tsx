cc68197c205973968ccbfadd2bc96f0e0f681f77
Jacek Michalski <jacmilski@gmail.com>
2024-04-03 02:43:26 +0200

    Add reusable header for dashboard and Subscriptions pages

--- app/(application)/(app)/dashboard/page.tsx
+++ app/(application)/(app)/dashboard/page.tsx
@@ -1,10 +1,12 @@
+import AppContentHeader from "@/components/AppContentHeader";
 import { UserButton } from "@clerk/nextjs";
 
 export default function DashboardPage() {
   return (
-    <div className="mx-0 px-4 text-center">
-      Hello in Dashboard
-      <UserButton />
-    </div>
+    <section className="h-screen p-8">
+      {/* TOP HEADER WITH BUTTONS */}
+      <AppContentHeader title="Dashboard" />
+      {/* CONTENT */}
+    </section>
   );
 }
